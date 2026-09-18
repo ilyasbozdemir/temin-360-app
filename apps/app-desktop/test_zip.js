94a7b89b23883ebc8f338979f5a6c5ca8bf12619
İlyas Bozdemir <bozdemir.ib70@gmail.com>
2026-09-18 19:45:50 +0300

    chore: release v1.0.0-beta.130

--- apps/app-desktop/test_zip.js
+++
@@ -1,9 +0,0 @@
-﻿const AdmZip = require('adm-zip')
-const fs = require('fs')
-
-fs.writeFileSync('temp_test/test.sqlite', 'data')
-fs.writeFileSync('temp_test/test.sqlite-wal', 'wal_data')
-
-const zip = new AdmZip()
-zip.addLocalFolder('temp_test')
-console.log(zip.getEntries().map((e) => e.entryName))
